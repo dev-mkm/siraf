5d464a3ef77db1105dec9c3a4377f14769747bde
dev-mkm <m.khoshdel81@gmail.com>
2022-06-02 11:21:43 +0430

    Main

--- chat/views.py
+++ chat/views.py
@@ -24,40 +24,41 @@ class ChatViewSet(viewsets.ViewSet):
     @action(detail=False, methods=['post'])
     def messageAdd(self, request, chat_id=None):
         if (chat_id == None):
-            content = {'message': 'ChatID cannot be empty'}
+            content = {'message': 'ChatID cannot be empty', 'status': 400}
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
         chats = Chat.objects.all()
         chat = get_object_or_404(chats, id=chat_id)
         if (request.user.status == True and chat.status == 'A'):
             if (chat.receiver_id != request.user.id and chat.sender_id != request.user.id):
-                content = {'message': 'Your not in this chat'}
+                content = {'message': 'Your not in this chat', 'status': 405}
                 return Response(content, status=status.HTTP_405_METHOD_NOT_ALLOWED)
             else:
                 message = Message(message=request.POST['message'], chat=chat, status=False, Creator_id=request.user)
                 message.save()
-                return Response(status=status.HTTP_200_OK)
+                content = {'status': 200}
+                return Response(content, status=status.HTTP_200_OK)
         else:
-            content = {'message': 'This Chat is Disabled'}
+            content = {'message': 'This Chat is Disabled', 'status': 405}
             return Response(content, status=status.HTTP_405_METHOD_NOT_ALLOWED)
     
     @action(detail=False, methods=['delete'])
     def message(self, request, chat_id=None, id=None):
         if (chat_id == None or id == None):
-            content = {'message': 'ChatID and ID cannot be empty'}
+            content = {'message': 'ChatID and ID cannot be empty', 'status': 400}
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
         chats = Message.objects.all()
         chat = get_object_or_404(chats, chat_id=chat_id, id=id)
         if (request.user.status == True and chat.status == 'A'):
             if (chat.receiver_id != request.user.id and chat.sender_id != request.user.id):
-                content = {'message': 'Your not in this chat'}
+                content = {'message': 'Your not in this chat', 'status': 405}
                 return Response(content, status=status.HTTP_405_METHOD_NOT_ALLOWED)
             else:
                 messages = Message.objects.all()
                 message = get_object_or_404(messages, chat_id=chat_id, id=id)
                 message.delete()
-                return Response(status=status.HTTP_200_OK)
+                content = {'status': 200}
+                return Response(content, status=status.HTTP_200_OK)
         else:
-            content = {'message': 'This Chat is Disabled'}
+            content = {'message': 'This Chat is Disabled', 'status': 405}
             return Response(content, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    
-    
+  
